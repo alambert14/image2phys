0b6090ff1f47eed0f5eaf4a1825f2326b99a6288
Andy Lambert <andyalambert@gmail.com>
2023-06-07 15:07:34 -0400

    6/7

---
+++ model.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from typing import Tuple
+
+
+class Model(nn.Module):
+    """
+
+    """
+    def __init__(self, image_size: Tuple[int, int]):
+
+        self.mlp = nn.Sequential(
+
+        )
+
+
+
+    def forward(self, image):
